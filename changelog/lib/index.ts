bbd89a53e2154767b27ff71804135a455c231db0
Kevin Nemec <kevin@mediatool.com>
2021-10-01 09:06:20 +0200

    fix(changelog-ui): Corrected misconfigured rollup

--- changelog/lib/index.ts
+++ changelog/lib/index.ts
@@ -1,3 +1,3 @@
 export * from './accordion'
 export * from './chip'
-export * from './event-item'
+export * from './common'
--- changelog/rollup.config.js
+++ changelog/rollup.config.js
@@ -2,6 +2,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
+import image from '@rollup/plugin-image'
 // import babel from '@rollup/plugin-babel'
 import pkg from './package.json'
 
@@ -25,6 +26,7 @@ export default {
     commonjs({
       ignoreGlobal: true,
     }),
+    image(),
     typescript({
       tsconfig: './tsconfig.build.json',
     }),
