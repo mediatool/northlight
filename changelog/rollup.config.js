391492d354c1b7f3ac5946f316e21c84099a87c2
Kevin Nemec <kevin@mediatool.com>
2021-10-01 15:37:53 +0200

    fix(changelog-ui): Output css

--- changelog/rollup.config.js
+++ changelog/rollup.config.js
@@ -1,8 +1,10 @@
-// eslint-disable
+/* eslint-disable import/no-extraneous-dependencies */
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import image from '@rollup/plugin-image'
+import postcss from 'rollup-plugin-postcss'
+import NpmImport from 'less-plugin-npm-import'
 // import babel from '@rollup/plugin-babel'
 import pkg from './package.json'
 
@@ -10,7 +12,7 @@ const external = [ ...Object.keys(pkg.devDependencies), ...Object.keys(pkg.depen
 const extensions = [ '.js', '.jsx', '.ts', '.tsx' ]
 
 export default {
-  input: './index.ts',
+  input: [ './index.ts', './lib/style.less' ],
   output: {
     format: 'esm',
     sourcemap: true,
@@ -29,6 +31,12 @@ export default {
     image(),
     typescript({
       tsconfig: './tsconfig.build.json',
+    }),
+    postcss({
+      use: [ [ 'less', {
+        plugins: [ new NpmImport({ prefix: '~' }) ],
+      } ] ],
+      extract: 'style.css',
     }),
   ],
 }
