040beb8462d8ef8039579e86f6c30fb9005a0062
Kevin Nemec <kevin@mediatool.com>
2021-11-17 11:24:19 +0100

    chore(ui/framework): Minimal theme override for button

--- framework/lib/theme/components/button/index.ts
+++ framework/lib/theme/components/button/index.ts
@@ -8,56 +8,15 @@ export const Button: ComponentSingleStyleConfig = {
     color: 'white',
     borderRadius: 2,
   },
-  sizes: {
-    xs: {
-      width: 55,
-      height: 24,
-      padding: [ 10, 8 ],
-    },
-    sm: {
-      width: 70,
-      height: 32,
-      padding: [ 10, 12 ],
-    },
-    md: {
-      width: 84,
-      height: 40,
-      padding: [ 10, 16 ],
-    },
-    lg: {
-      width: 107,
-      height: 48,
-      padding: [ 10, 24 ],
-    },
-  },
   variants: {
-    solid: {
-      bg: 'primary',
-      color: 'white',
+    solid: (props) => ({
       _hover: {
-        bg: whiten('mediatoolBlue', 10),
+        bg: whiten(props.colorScheme, 10),
       },
-    },
-    outlined: {
-      bg: 'transparent',
-      color: 'primary',
-      border: '1px solid',
-      borderColor: 'primary',
-      _hover: {
-        bg: 'blue.50',
-      },
-    },
-    ghost: {
-      bg: 'transparent',
-      color: 'primary',
-      border: 'none',
-      _hover: {
-        boxShadow: 'blue.50',
-      },
-    },
+    }),
   },
   defaultProps: {
-    colorScheme: 'blue',
+    colorScheme: 'mediatoolBlue',
     variant: 'solid',
     size: 'md',
   },
