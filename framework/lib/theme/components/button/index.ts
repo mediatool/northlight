ef49b4c1c90d571fb5e5d90df459fbb7468f74c8
Magnus <ma.ohlin@gmail.com>
2021-12-01 08:33:34 +0100

    chore(ui/framework): Finalize button styling
    This adds the styling from the DS to buttons in the ui framework

--- framework/lib/theme/components/button/index.ts
+++ framework/lib/theme/components/button/index.ts
@@ -1,19 +1,46 @@
 import {
   ComponentSingleStyleConfig,
 } from '@chakra-ui/react'
-import { lighten } from '@chakra-ui/theme-tools'
+import { whiten } from '@chakra-ui/theme-tools'
 
 export const Button: ComponentSingleStyleConfig = {
   baseStyle: {
     color: 'white',
-    borderRadius: 2,
+    borderRadius: 6,
   },
   variants: {
     solid: ({ theme, colorScheme }) => ({
       _hover: {
-        bg: lighten(`${colorScheme}.500`, 10)(theme),
+        bg: whiten(`${colorScheme}.500`, 10)(theme),
+        _disabled: {
+          bg: 'mediatoolBlue.100',
+          opacity: 100,
+        },
+      },
+      _disabled: {
+        bg: 'mediatoolBlue.100',
+        opacity: 100,
       },
     }),
+    outline: {
+      borderWidth: 2,
+      _disabled: {
+        color: 'mediatoolBlue.100',
+        opacity: 100,
+      },
+    },
+    ghost: {
+      _disabled: {
+        color: 'mediatoolBlue.100',
+        opacity: 100,
+      },
+    },
+    link: {
+      _disabled: {
+        color: 'mediatoolBlue.100',
+        opacity: 100,
+      },
+    },
   },
   defaultProps: {
     colorScheme: 'mediatoolBlue',
--- framework/sandbox/docs/pages/button-page/index.tsx
+++ framework/sandbox/docs/pages/button-page/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, HStack } from '~lib/components'
+import { HubIcon, SwitchesIcon } from '~lib/components/icons'
 import { DefaultPageSubtitle, Page } from '../../components'
 
 const ButtonPage = () => (
@@ -17,6 +18,48 @@ const ButtonPage = () => (
       <Button variant="outline">Primary outlined</Button>
       <Button variant="ghost">Primary ghost</Button>
       <Button variant="link">Primary link</Button>
+    </HStack>
+    <HStack spacing={ 4 } mt={ 4 }>
+      <Button disabled={ true }>Solid disabled</Button>
+      <Button disabled={ true } variant="outline">Outlined disabled</Button>
+      <Button disabled={ true } variant="ghost">Ghost disabled</Button>
+      <Button disabled={ true } variant="link">Link disabled</Button>
+    </HStack>
+    <HStack spacing={ 4 } mt={ 4 }>
+      <Button colorScheme="red">Red solid</Button>
+      <Button colorScheme="red" variant="outline">Red outlined</Button>
+      <Button colorScheme="red" variant="ghost">Red ghost</Button>
+      <Button colorScheme="red" variant="link">Red link</Button>
+    </HStack>
+    <HStack spacing={ 4 } mt={ 4 }>
+      <Button size="xs">Solid xs</Button>
+      <Button size="sm">Solid sm</Button>
+      <Button size="md">Solid md</Button>
+      <Button size="lg">Solid lg</Button>
+    </HStack>
+    <HStack spacing={ 4 } mt={ 4 }>
+      <Button size="xs" variant="outline">Outlined xs</Button>
+      <Button size="sm" variant="outline">Outlined sm</Button>
+      <Button size="md" variant="outline">Outlined md</Button>
+      <Button size="lg" variant="outline">Outlined lg</Button>
+    </HStack>
+    <HStack spacing={ 4 } mt={ 4 }>
+      <Button size="xs" variant="ghost">Ghost xs</Button>
+      <Button size="sm" variant="ghost">Ghost sm</Button>
+      <Button size="md" variant="ghost">Ghost md</Button>
+      <Button size="lg" variant="ghost">Ghost lg</Button>
+    </HStack>
+    <HStack spacing={ 4 } mt={ 4 }>
+      <Button leftIcon={ <HubIcon /> }>Primary solid icon</Button>
+      <Button leftIcon={ <HubIcon /> } variant="outline">Primary outlined icon</Button>
+      <Button leftIcon={ <HubIcon /> } variant="ghost">Primary ghost icon</Button>
+      <Button leftIcon={ <HubIcon /> } variant="link">Primary link icon</Button>
+    </HStack>
+    <HStack spacing={ 4 } mt={ 4 }>
+      <Button rightIcon={ <SwitchesIcon /> }>Primary solid icon</Button>
+      <Button rightIcon={ <SwitchesIcon /> } variant="outline">Primary outlined icon</Button>
+      <Button rightIcon={ <SwitchesIcon /> } variant="ghost">Primary ghost icon</Button>
+      <Button rightIcon={ <SwitchesIcon /> } variant="link">Primary link icon</Button>
     </HStack>
   </Page>
 )
