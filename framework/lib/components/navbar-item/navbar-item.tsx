cc6060726d9def1889698452fe96ae5c5f8d7def
paolo <paolo@mediatool.com>
2022-04-22 09:30:02 +0200

    feat(ui): add navbarItem component

---
+++ framework/lib/components/navbar-item/navbar-item.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { LinkProps, NavLink, NavLinkProps } from 'react-router-dom'
+import { IconType } from '~/lib'
+import { Center, Icon, Link, Stack, Text } from '~lib/components'
+
+type NavbarItemProps = LinkProps & NavLinkProps & {
+  to: string
+  icon: IconType
+  renderTitle?: boolean
+}
+
+export const NavbarItem = ({
+  to,
+  icon,
+  title,
+  renderTitle = true,
+}: NavbarItemProps) => (
+  <Stack align="center" spacing={ 1 }>
+    <Link
+      to={ to }
+      as={ NavLink }
+      borderRadius={ 4 }
+      _hover={ { bgColor: 'blue.100' } }
+      _focus={ { outline: 'none' } }
+      _activeLink={ { bgColor: 'blue.100' } }
+    >
+      <Center boxSize={ 46 } >
+        <Icon boxSize={ 5 } type={ icon } />
+      </Center>
+    </Link>
+    { renderTitle &&
+      <Text fontSize="xs" color="mediatoolBlue.500">{ title }</Text>
+    }
+  </Stack>
+)
