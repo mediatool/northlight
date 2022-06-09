4b37d775e06ac2df291ea4580c303f616a01b828
paolo <paolo@mediatool.com>
2022-06-09 15:48:54 +0200

    chore(ui): bump Chakra and React
    Chakra UI v2 added some good stuff but it requires React 18

--- framework/sandbox.tsx
+++ framework/sandbox.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import * as ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { MediatoolThemeProvider } from './lib/components'
 import { Routing, sandboxRoutes } from './sandbox/routing'
 
-ReactDOM.render(
-  <MediatoolThemeProvider>
-    <BrowserRouter>
-      <Routing fallback="/docs" routes={ sandboxRoutes } />
-    </BrowserRouter>
-  </MediatoolThemeProvider>,
-  document.querySelector('#app')
-)
+ReactDOM
+  .createRoot(document.getElementById('app')!)
+  .render(
+    <MediatoolThemeProvider>
+      <BrowserRouter>
+        <Routing fallback="/docs" routes={ sandboxRoutes } />
+      </BrowserRouter>
+    </MediatoolThemeProvider>
+  )
--- framework/sandbox/docs/pages/checkbox-page/index.tsx
+++ framework/sandbox/docs/pages/checkbox-page/index.tsx
@@ -16,7 +16,7 @@ const CheckboxPage = () => (
       <Checkbox size="sm" value="1">Small checkbox</Checkbox>
       <Checkbox value="2">Medium checkbox (default)</Checkbox>
       <Checkbox size="lg" value="3">Large checkbox</Checkbox>
-      <Checkbox colorScheme="purple" value="4" defaultIsChecked={ true } isDisabled={ true }>Purple color scheme checkbox</Checkbox>
+      <Checkbox colorScheme="purple" value="4" defaultChecked={ true } isDisabled={ true }>Purple color scheme checkbox</Checkbox>
       <Checkbox colorScheme="green" value="5" isInvalid={ true }>Green color scheme checkbox</Checkbox>
     </HStack>
   </Page>
