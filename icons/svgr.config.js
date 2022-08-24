aeb71a29b7ff718f8f208e2d53b5effe54fe3914
paolo <paolo@mediatool.com>
2022-08-24 11:12:24 +0200

    feat(ui): add icons package
    Added package for the icons. The idea is to import svgs files in the
    assets and build icon components to export and use in the webapp

---
+++ icons/svgr.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  icon: true,
+  titleProp: true,
+  replaceAttrValues: {
+    '#111': 'currentColor',
+    '#000': 'currentColor',
+    '#fff': 'rgba(0,0,0,0)',
+  },
+  typescript: true,
+}
