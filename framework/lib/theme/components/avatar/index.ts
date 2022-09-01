fa351fbe08c34b16056a1d617023fd423cc2818c
paolo <paolo@mediatool.com>
2022-09-01 13:16:53 +0200

    fix(ui/framework): fix sizing for avatar
    The md avatar was too big so we tokenized and modified the theme

--- framework/lib/theme/components/avatar/index.ts
+++ framework/lib/theme/components/avatar/index.ts
@@ -1,8 +1,46 @@
 import { ComponentMultiStyleConfig } from '@chakra-ui/react'
-import { borderRadius, color } from '@mediatool/tokens'
+import { borderRadius, color, sizing } from '@mediatool/tokens'
 
 export const Avatar: ComponentMultiStyleConfig = {
   parts: [ 'container' ],
+  sizes: {
+    '2xs': {
+      container: {
+        boxSize: sizing.avatar['2xs'],
+      },
+    },
+    xs: {
+      container: {
+        boxSize: sizing.avatar.xs,
+      },
+    },
+    sm: {
+      container: {
+        boxSize: sizing.avatar.sm,
+      },
+    },
+    md: {
+      container: {
+        boxSize: sizing.avatar.md,
+      },
+    },
+    lg: {
+      container: {
+        boxSize: sizing.avatar.lg,
+      },
+    },
+    xl: {
+      container: {
+        boxSize: sizing.avatar.xl,
+      },
+    },
+    '2xl': {
+      container: {
+        boxSize: sizing.avatar['2xl'],
+      },
+    },
+
+  },
   baseStyle: {
     container: {
       bgColor: color.background.avatar.default,
