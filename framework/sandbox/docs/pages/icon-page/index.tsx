0909b05bb5b0a85a9eaac49ac64df2a2a0381661
paolo <paolo@mediatool.com>
2022-09-02 10:09:18 +0200

    chore(ui/framework): add documentation for icons
    Add an explanation on how to use the new icons

--- framework/sandbox/docs/pages/icon-page/index.tsx
+++ framework/sandbox/docs/pages/icon-page/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import * as icons from '@mediatool/icons'
 import {
+  Code,
   Flex,
   Icon,
   SimpleGrid,
+  Stack,
   Text,
 } from '../../../../lib/components'
 import { Page } from '../../components'
@@ -15,6 +17,18 @@ const IconPage = () => {
       title="Icon"
       subtitle="Custom icons imported from @mediatool/icons"
     >
+      <Stack spacing={ 4 }>
+        <Text>Mediatool icons are exported as components from the package{ ' ' }
+          <Code>@mediatool/icons</Code>
+        </Text>
+        <Text>Mediatool uses 2 variants of icons:
+          <strong> solid</strong> and <strong>duo</strong>(duotone)
+        </Text>
+        <Text>Every icon is exported in these 2 variants, as you can see from the names.</Text>
+        <Text>To use an icon, you need to pass it to the icon component like this:</Text>
+        <Code w="max-content">{ '<Icon as={ IconFromTheLibrary }' }</Code>
+        <Text />
+      </Stack>
       <SimpleGrid mt={ 6 } columns={ 6 } w="100%" maxW={ 1200 }>
         { mediatoolIcons.map((iconType) => (
           <Flex
