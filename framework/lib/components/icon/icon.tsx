b4f338b84c502528bd0b027e16cb06457fb15897
paolo <paolo@mediatool.com>
2022-09-27 16:26:56 +0200

    feat(ui/framework): add theme for icon
    Added theme for the Icon component. It has also default sizes to make it easier to integrate with the iconButton component

--- framework/lib/components/icon/icon.tsx
+++ framework/lib/components/icon/icon.tsx
@@ -3,22 +3,28 @@ import {
   Icon as ChakraIcon,
   IconProps as ChakraIconProps,
   forwardRef,
+  useStyleConfig,
 } from '@chakra-ui/react'
 
 type Props = ChakraIconProps & {
   as?: ComponentType<any>
+  size?: 'xs' | 'sm' | 'md' | 'lg'
 }
 
 export const Icon = forwardRef(({
-  as: As, ...rest
+  as: As,
+  size,
+  color,
+  boxSize,
+  ...rest
 }: Props, ref: any) => {
   const CustomIcon = As ?? (() => <ChakraIcon />)
+  const styles = useStyleConfig('Icon', { size, color, boxSize })
 
   return (
     <ChakraIcon
       ref={ ref }
-      color={ rest.color ?? 'unset' }
-      boxSize={ 5 }
+      sx={ styles }
       { ...rest }
     >
       <CustomIcon />
--- framework/lib/theme/components/icon/index.ts
+++ framework/lib/theme/components/icon/index.ts
@@ -1,12 +1,23 @@
 import { ComponentSingleStyleConfig } from '@chakra-ui/react'
-import { color } from '@mediatool/tokens'
+import { sizing } from '@mediatool/tokens'
 
 export const Icon: ComponentSingleStyleConfig = {
-  baseStyle: ({ color: PropColor }) => ({
-    color: PropColor,
-    boxSize: 6,
-  }),
-  defaultProps: {
-    color: color.text.default,
+  sizes: {
+    xs: {
+      boxSize: sizing.icon.xs,
+    },
+    sm: {
+      boxSize: sizing.icon.sm,
+    },
+    md: {
+      boxSize: sizing.icon.md,
+    },
+    lg: {
+      boxSize: sizing.icon.lg,
+    },
   },
+  baseStyle: ({ boxSize, color }) => ({
+    boxSize: boxSize || sizing.icon.default,
+    color: color || 'unset',
+  }),
 }
