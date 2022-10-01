f2eabac03a0214cf9b400b367b9e04974e82a313
Sebastian Delgado <sebastian.delgado@mediatool.com>
2022-10-01 18:22:40 +0200

    feat(ui): Improved utility getContrastColor
    Previousely getContrastColor was written with a threshold of 128,
    this was however inaccuarate as the dark text color had a brightness
    of 38.6 and not 0, it has now been rewriten more rigoursely to
    adapt for this.

--- framework/lib/utilities/getContrastColor/index.ts
+++ framework/lib/utilities/getContrastColor/index.ts
@@ -1,21 +1,10 @@
 import { color as colorToken } from '@mediatool/tokens'
+import { luminosity } from '../luminosity'
 
-export function getContrastColor (hexcolor: string) {
-  let color = hexcolor
+export const getContrastColor = (color: string) => {
+  const brightColor = colorToken.text.inverted
+  const darkColor = colorToken.text.default
+  const threshold = (luminosity(darkColor) + luminosity(brightColor)) / 2
 
-  if (color.slice(0, 1) === '#') {
-    color = color.slice(1)
-  }
-
-  if (color.length === 3) {
-    color = color.split('').map((hex) => hex + hex).join('')
-  }
-
-  const r = parseInt(color.substring(0, 2), 16)
-  const g = parseInt(color.substring(2, 4), 16)
-  const b = parseInt(color.substring(4, 6), 16)
-
-  const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000
-
-  return (yiq >= 128) ? colorToken.text.default : colorToken.text.inverted
+  return luminosity(color) >= threshold ? darkColor : brightColor
 }
