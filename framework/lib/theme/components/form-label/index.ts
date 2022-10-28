101a8fe8a9245aa3506d1828388dbb1f248b5b2e
paolo <paolo@mediatool.com>
2022-10-28 10:28:16 +0200

    fix(ui/framework): update form labels

--- framework/lib/theme/components/form-label/index.ts
+++ framework/lib/theme/components/form-label/index.ts
@@ -1,18 +1,18 @@
 import { ComponentStyleConfig } from '@chakra-ui/react'
 import { color, typography } from '@mediatool/tokens'
 
-const { largeLabel } = typography.labels
+const { smLabel } = typography.labels
 
 export const FormLabel: ComponentStyleConfig = {
   baseStyle: {
     color: color.text.default,
     width: '80%',
     maxWidth: '45ch',
-    fontFamily: largeLabel.fontFamily,
-    fontWeight: largeLabel.fontWeight,
-    lineHeight: largeLabel.lineHeight,
-    fontSize: largeLabel.fontSize,
-    letterSpacing: largeLabel.letterSpacing,
-    textTransform: largeLabel.textCase,
+    fontFamily: smLabel.fontFamily,
+    fontWeight: smLabel.fontWeight,
+    lineHeight: smLabel.lineHeight,
+    fontSize: smLabel.fontSize,
+    letterSpacing: smLabel.letterSpacing,
+    textTransform: smLabel.textCase,
   },
 }
