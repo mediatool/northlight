f10ba6546bb4c79437393f9c85553ba8ec45499b
Sebastian Delgado <sebastian.delgado@mediatool.com>
2022-12-19 11:08:17 +0100

    fix(ui): getContrastColor

--- framework/lib/utils/get-contrast-color/index.ts
+++ framework/lib/utils/get-contrast-color/index.ts
@@ -4,6 +4,7 @@ import { luminosity } from '../luminosity'
 export const getContrastColor = (color: string) => {
   const colorTwo = useToken('colors', 'text.default')
   const colorOne = useToken('colors', 'text.inverted')
+  const colorInHex = useToken('colors', color)
 
   const l1 = luminosity(colorOne)
   const l2 = luminosity(colorTwo)
@@ -13,5 +14,5 @@ export const getContrastColor = (color: string) => {
   const brightColor = l1 > l2 ? colorOne : colorTwo
   const darkColor = l1 > l2 ? colorTwo : colorOne
 
-  return luminosity(color) >= threshold ? darkColor : brightColor
+  return luminosity(colorInHex) >= threshold ? darkColor : brightColor
 }
