cd90d112a5027948a908c9c83422be2004f5924a
Sebastian Delgado <sebastian.delgado@mediatool.com>
2022-12-22 17:09:47 +0100

    fix(ui): date picker value validation

--- framework/lib/components/date-picker/date-picker-field/utils.ts
+++ framework/lib/components/date-picker/date-picker-field/utils.ts
@@ -1,15 +1,9 @@
-import { has, is, test } from 'ramda'
-
-const validDateString = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/gm
-
-export const isValidDateString = test(validDateString)
+import { has, is } from 'ramda'
 
 export const isValidDateRange = (value: unknown) => (
   is(Object, value) &&
   has('startDate', value) &&
   has('endDate', value) &&
   is(String, value.startDate) &&
-  is(String, value.endDate) &&
-  isValidDateString(value.startDate) &&
-  isValidDateString(value.endDate)
+  is(String, value.endDate)
 )
