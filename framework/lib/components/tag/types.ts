030b1d14237665999ac8753566dc42d80ac85b8c
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-02-16 20:32:40 +0100

    feat(framework): tag-group

--- framework/lib/components/tag/types.ts
+++ framework/lib/components/tag/types.ts
@@ -1,6 +1,11 @@
-import { TagProps as ChakraTagProps } from '@chakra-ui/react'
+import { TagProps as ChakraTagProps, StackProps } from '@chakra-ui/react'
 
 export interface TagProps extends ChakraTagProps {
   bgColor?: string
   variant?: 'solid' | 'subtle'
+}
+export interface TagGroupProps extends StackProps {
+  max?: number
+  spacing?: number | string
+  children: JSX.Element[] | React.ReactNode[]
 }
