e1adaff6cf718951d14ac17bd5320de3cf039767
Ludwig Magnusson <ludwig@mediatool.com>
2023-02-21 15:23:39 +0100

    chore: upgrade almost all dependencies
    Ramda and react-router-dom held back.

--- framework/lib/components/transitions/types.ts
+++ framework/lib/components/transitions/types.ts
@@ -5,13 +5,15 @@ import {
   SlideFadeProps as ChakraSlideFadeProps,
   SlideProps as ChakraSlideProps,
 } from '@chakra-ui/react'
+import { ReactNode } from 'react'
 
 export type DurationType = number | null
 export type OffsetType = number | string
 export type ChildrenType = React.ReactNode | React.ReactNode[]
 export type TransitionDirection = 'right' | 'bottom' | 'left' | 'top' | null
 
-export interface FadeProps extends ChakraFadeProps {
+export interface FadeProps extends Omit<ChakraFadeProps, 'children' > {
+  children: ReactNode | ReactNode[]
   duration?: DurationType
   enterDuration?: DurationType
   exitDuration?: DurationType
@@ -22,12 +24,12 @@ export interface FadeProps extends ChakraFadeProps {
   onTransitionComplete?: (isHidden: boolean) => void
 }
 
-export interface ScaleFadeProps extends FadeProps, Omit<ChakraScaleFadeProps, 'delay'> {}
+export interface ScaleFadeProps extends FadeProps, Omit<ChakraScaleFadeProps, 'delay' | 'children'> {}
 
-export interface SlideProps extends FadeProps, Omit<ChakraSlideProps, 'delay'> {}
-export interface SlideFadeProps extends FadeProps, Omit<ChakraSlideFadeProps, 'delay'> {
+export interface SlideProps extends FadeProps, Omit<ChakraSlideProps, 'delay' | 'children'> {}
+export interface SlideFadeProps extends FadeProps, Omit<ChakraSlideFadeProps, 'delay' | 'children'> {
   direction?: TransitionDirection
   offset?: number
 }
 
-export interface CollapseProps extends FadeProps, Omit<ChakraCollapseProps, 'delay'> {}
+export interface CollapseProps extends FadeProps, Omit<ChakraCollapseProps, 'delay' | 'children'> {}
