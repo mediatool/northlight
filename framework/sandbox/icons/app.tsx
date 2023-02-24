e60011e80f0cca3e4f2c9e572eeb1c577b0d382e
ilija- <ilijatp@gmail.com>
2023-02-24 18:09:25 +0100

    chore(northlight/icons):Page facelift, Navigation in icons updated

--- framework/sandbox/icons/app.tsx
+++ framework/sandbox/icons/app.tsx
@@ -1,26 +1,65 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { Code, Divider, H1, Link, P, Stack, Text } from '../../lib'
+import { Link as NavLink } from 'react-router-dom'
+import { Code, Divider, H1, P, Icon, Stack, Text, Card, CardHeader, CardBody, CardFooter, SimpleGrid, H3, Center, Heading, Link, Spacer } from '../../lib'
+import { BrightnessSolid, BrightnessDuo, SmileyDuo } from '@northlight/icons'
 
 const Icons = () => (
   <Stack p={ 4 } spacing={ 4 } bgColor="background.default" height="auto">
-    <H1>Mediatool icons</H1>
-    <P>Exported as components from the package{ ' ' }
+    <H1>Icons <Icon as={ SmileyDuo } sx={{ color: 'blue.500' }} /> </H1>
+    <P>Northlight provides a set of commonly used interface icons you can use in your project, exported as components from the package{ ' ' }
       <Code>@northlight/icons</Code>
     </P>
-    <Divider />
-    <Stack>
-      <P>Mediatool uses 2 variants of icons: { ' ' }
-        <Link as={ NavLink } color="blue.500" fontWeight="bold" to="/icons/solid">solid</Link> and { ' ' }
-        <Link as={ NavLink } color="blue.500" fontWeight="bold" to="/icons/duo">duo</Link>.
+
+    <Center>
+      <H3 mt={ 5 } mb={ 5 }>Select Variant</H3>
+    </Center>
+    <SimpleGrid spacing={ 10 } alignItems="center" minChildWidth="300px" templateColumns='repeat(auto-fit, minmax(200px, 1fr))'>
+
+    <Spacer />
+      <Link as={ NavLink }  sx={ { borderRadius: 'lg',  _hover: { textDecoration: 'none', bgColor: 'blue.500', color: 'gray.50' } } } to="/icons/duo">
+        <Card align='center' variant='outline' sx={ { _hover: { color: 'mono.white', borderColor: 'blue.500' } } }>
+          <CardHeader>
+          <Heading size='md'> Duo </Heading>
+          </CardHeader>
+          <CardBody>
+          <Icon as={ BrightnessDuo } boxSize={ 50 } />
+          </CardBody>
+          <CardFooter>
+          </CardFooter>
+        </Card>
+      </Link>
+
+      <Link as={ NavLink } sx={ { borderRadius: 'lg', _hover: { textDecoration: 'none', bgColor: 'blue.500', color: 'gray.50' } } } to="/icons/solid">
+        <Card align='center' variant='outline' sx={ { _hover: { color: 'mono.white', borderColor: 'blue.500' } } }>
+          <CardHeader>
+            <Heading size='md'> Solid </Heading>
+            </CardHeader>
+            <CardBody>
+            <Icon as={ BrightnessSolid } boxSize={ 50 } />
+            </CardBody>
+            <CardFooter>
+            </CardFooter>
+        </Card>
+      </Link>
+      <Spacer />
+
+    </SimpleGrid>
+
+    <Stack pt={ 10 }>
+      <P>Northlight uses 2 variants of icons: { ' ' }
+      <Link as={ NavLink } color="blue.500" fontWeight="bold" to="/icons/solid">solid</Link> and { ' ' }
+      <Link as={ NavLink } color="blue.500" fontWeight="bold" to="/icons/duo">duo</Link>.
+      Every icon is exported in both variants.
       </P>
-      <P>Every icon is exported in both variants.</P>
     </Stack>
+    
     <Divider />
+    
     <Stack>
-      <Text>To use an icon, you need to pass it to the icon component like this:</Text>
+      <Text>To use an icon, you need to pass it to the icon component:</Text>
       <Code w="max-content">{ '<Icon as={ IconFromTheLibrary } />' }</Code>
     </Stack>
+    
   </Stack>
 
 )
--- framework/sandbox/icons/utils/icons-page.tsx
+++ framework/sandbox/icons/utils/icons-page.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { isEmpty } from 'ramda'
+import { Link as NavLink, Route } from 'react-router-dom'
 import { IconCard, IconsGrid } from '../components'
-import { Box, H3, Input, VStack } from '../../../lib'
+import { Box, H2, H3, Input, VStack, HStack, IconButton, Icon, } from '../../../lib'
 import { useSortedIcons } from './use-sorted-icons'
 import { IconPageProps } from './types'
+import { BrightnessSolid, BrightnessDuo, ChevronLeftDuo } from '@northlight/icons'
+
 
 export const IconPage = ({ iconVariant }: IconPageProps) => {
   const { sortedIcons, debouncedHandleChange } = useSortedIcons(iconVariant)
   return (
     <VStack>
+      <HStack>
+      <NavLink to="/icons">
+      <IconButton variant="brand" aria-label="Selection" icon={ <Icon as={ ChevronLeftDuo } /> } />
+      </NavLink>
       <Box w="sm">
-        <Input onChange={ debouncedHandleChange } placeholder="Sort Icons" />
+        <Input onChange={ debouncedHandleChange } placeholder="Search icons.." />
       </Box>
+      </HStack>
+
+      <Box pt={ 10 } mb={ 10 }>    
+        <Route path={["/icons/duo"]} render={()=><H2><Icon as={ BrightnessDuo } /> Duo</H2>} /> 
+        <Route path={["/icons/solid"]} render={()=><H2><Icon as={ BrightnessSolid } /> Solid</H2>} /> 
+      </Box>
+
       <IconsGrid>
         { sortedIcons.map((icon) => (
           <IconCard key={ icon.label } { ...icon } />
