a618c301a44faf553a17660f00ffa9f7ad2318e5
ilija- <ilijatp@gmail.com>
2023-02-27 11:44:54 +0100

    lint setup test

--- framework/sandbox/icons/utils/icons-page.tsx
+++ framework/sandbox/icons/utils/icons-page.tsx
@@ -1,29 +1,28 @@
 import React from 'react'
 import { isEmpty } from 'ramda'
 import { Link as NavLink, Route } from 'react-router-dom'
+import { BrightnessDuo, BrightnessSolid, ChevronLeftDuo } from '@northlight/icons'
 import { IconCard, IconsGrid } from '../components'
-import { Box, H2, H3, Input, VStack, HStack, IconButton, Icon, } from '../../../lib'
+import { Box, H2, H3, HStack, Icon, IconButton, Input, VStack } from '../../../lib'
 import { useSortedIcons } from './use-sorted-icons'
 import { IconPageProps } from './types'
-import { BrightnessSolid, BrightnessDuo, ChevronLeftDuo } from '@northlight/icons'
-
 
 export const IconPage = ({ iconVariant }: IconPageProps) => {
   const { sortedIcons, debouncedHandleChange } = useSortedIcons(iconVariant)
   return (
     <VStack>
       <HStack>
-      <NavLink to="/icons">
-      <IconButton variant="brand" aria-label="Selection" icon={ <Icon as={ ChevronLeftDuo } /> } />
-      </NavLink>
-      <Box w="sm">
-        <Input onChange={ debouncedHandleChange } placeholder="Search icons.." />
-      </Box>
+        <NavLink to="/icons">
+          <IconButton variant="brand" aria-label="Selection" icon={ <Icon as={ ChevronLeftDuo } /> } />
+        </NavLink>
+        <Box w="sm">
+          <Input onChange={ debouncedHandleChange } placeholder="Search icons.." />
+        </Box>
       </HStack>
 
-      <Box pt={ 10 } mb={ 10 }>    
-        <Route path={["/icons/duo"]} render={()=><H2><Icon as={ BrightnessDuo } /> Duo</H2>} /> 
-        <Route path={["/icons/solid"]} render={()=><H2><Icon as={ BrightnessSolid } /> Solid</H2>} /> 
+      <Box pt={ 10 } mb={ 10 }>
+        <Route path={ [ '/icons/duo' ] } render={ () => <H2><Icon as={ BrightnessDuo } /> Duo</H2> } />
+        <Route path={ [ '/icons/solid' ] } render={ () => <H2><Icon as={ BrightnessSolid } /> Solid</H2> } />
       </Box>
 
       <IconsGrid>
