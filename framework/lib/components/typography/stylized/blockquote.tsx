f0d65710d0c78401ab893fce26f9ffae4cb2d4bf
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-03-29 22:46:22 +0200

    added blockquote page

--- framework/lib/components/typography/stylized/blockquote.tsx
+++ framework/lib/components/typography/stylized/blockquote.tsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import { Text as ChakraText, TextProps, useStyleConfig } from '@chakra-ui/react'
 
-export const Blockquote = ({
-  children,
-  sx = {},
-  ...rest
-}: TextProps) => {
+/**
+ * One of the stylized typography components
+ * @see Capitalized
+ * @see Lead
+ * @see Small
+ * @see Tiny
+ * @see {@link https://northlight.dev/reference/blockquote }
+ *
+ * @example (Example)
+ * Example:
+ * (?
+ * <Blockquote sx={{color: 'green.500' }}>Success</Blockquote>
+ * ?)
+ * (_Note, all other css properties not listed
+ * under the props tab to right should be passed down via **sx**_)
+ *
+ *
+ *
+ */
+export const Blockquote = ({ children, sx = {}, ...rest }: TextProps) => {
   const styles = useStyleConfig('Blockquote', { sx })
 
   return (
-    <ChakraText
-      as="span"
-      sx={ styles }
-      { ...rest }
-    >
+    <ChakraText as="span" sx={ styles } { ...rest }>
       { children }
     </ChakraText>
   )
