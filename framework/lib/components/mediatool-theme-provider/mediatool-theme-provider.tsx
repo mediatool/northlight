487c2947fd3f46f05cb3c8ac54674a22475b610c
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-03-29 22:03:54 +0200

    delete!!!

--- framework/lib/components/mediatool-theme-provider/mediatool-theme-provider.tsx
+++ framework/lib/components/mediatool-theme-provider/mediatool-theme-provider.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
-import { theme as webappTheme } from '../../theme'
+import { tottTheme, theme as webappTheme } from '../../theme'
 import { MediatoolThemeProviderProps } from './types'
 import { CustomTheme } from '../../utils/use-current-theme'
 
 export const MediatoolThemeProvider = ({
   children,
+<<<<<<< HEAD
   theme = webappTheme,
   themeName = 'webappTheme',
+=======
+  theme = tottTheme,
+>>>>>>> 137a4b0c6 (delete!!!)
 }: MediatoolThemeProviderProps) => (
   <ChakraProvider resetCSS={ true } theme={ theme }>
     <CustomTheme.Provider value={ themeName }>
--- framework/sandbox/reference/pages/mediatool-theme-provider-page/index.tsx
+++ framework/sandbox/reference/pages/mediatool-theme-provider-page/index.tsx
@@ -4,7 +4,7 @@ import ReferencePage from '../../reference-page'
 
 
 const MediatoolThemeProviderPage = () => (
-  <ReferencePage data={{"tags":{},"filePath":"lib/components/mediatool-theme-provider/mediatool-theme-provider.tsx","description":"","displayName":"MediatoolThemeProvider","methods":[],"props":{"theme":{"defaultValue":{"value":"extendTheme(overrides, TottSkin)"},"description":"","name":"theme","parent":{"fileName":"framework/lib/components/mediatool-theme-provider/types.ts","name":"MediatoolThemeProviderProps"},"declarations":[{"fileName":"framework/lib/components/mediatool-theme-provider/types.ts","name":"MediatoolThemeProviderProps"}],"required":false,"type":{"name":"Record<string, any>"}}}}} />
+  <ReferencePage data={{"tags":{},"filePath":"lib/components/mediatool-theme-provider/mediatool-theme-provider.tsx","description":"","displayName":"MediatoolThemeProvider","methods":[],"props":{"theme":{"defaultValue":{"value":"extendTheme(overrides, WebappSkin)"},"description":"","name":"theme","parent":{"fileName":"framework/lib/components/mediatool-theme-provider/types.ts","name":"MediatoolThemeProviderProps"},"declarations":[{"fileName":"framework/lib/components/mediatool-theme-provider/types.ts","name":"MediatoolThemeProviderProps"}],"required":false,"type":{"name":"Record<string, any>"}}}}} />
 );
 
 export default MediatoolThemeProviderPage
