6a9b7d9947d021498bf6caafc019dc0f19a8f38a
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-03-29 22:58:47 +0200

    button docs

--- framework/lib/components/button/button.tsx
+++ framework/lib/components/button/button.tsx
@@ -2,12 +2,43 @@ import React, { forwardRef } from 'react'
 import { Button as ChakraButton } from '@chakra-ui/react'
 import { ButtonProps } from './types'
 
-export const Button = forwardRef(({
-  variant = 'default',
-  children,
-  ...rest
-}: ButtonProps, ref: any) => (
-  <ChakraButton variant={ variant } ref={ ref } { ...rest }>
-    { children }
-  </ChakraButton>
-))
+/**
+ * @see {@link https://northlight.dev/reference/button}
+ *
+ * @example (Example)
+ * Example:
+ * (?
+ * +const variants = ['link', 'success', 'default', 'danger', 'brand', 'ghost']
+ *
+ * const ExampleButton = () => {
+ *  const [currentVariant, setCurrentVariant ] = useState(0)
+ *  const [ isLoading, setIsLoading ] = useState(false)
+ *
+ * const handleClick = () => {
+ * setIsLoading(true)
+ * setTimeout(() => {
+ * setCurrentVariant((prev) => prev === 5 ? 0 : prev + 1)
+ * setIsLoading(false)
+ * }, 400)
+ *
+ * }
+ *
+ *  return (
+ * <Button variant={variants[currentVariant]}
+ * isLoading={isLoading} onClick={handleClick} loadingText="Saving...">
+ * Save changes
+ * </Button>
+ * )
+ *
+ * }
+ *
+ * render(<ExampleButton/>)
+ * ?)
+ */
+export const Button = forwardRef(
+  ({ variant = 'default', children, ...rest }: ButtonProps, ref: any) => (
+    <ChakraButton variant={ variant } ref={ ref } { ...rest }>
+      { children }
+    </ChakraButton>
+  )
+)
