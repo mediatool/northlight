1e7afcb487ee0aea4e76983ed71064606c7d959d
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-03-29 22:40:18 +0200

    blinker page

--- framework/lib/components/blinker/blinker.tsx
+++ framework/lib/components/blinker/blinker.tsx
@@ -3,6 +3,18 @@ import { useStyleConfig } from '@chakra-ui/react'
 import { Box } from '../box'
 import { BlinkerProps } from './types'
 
+/**
+ * Status indicator of some sorts
+ * @see {@link https://northlight.dev/reference/blinker}
+ *
+ * @example (Example)
+ * Example:
+ * (?
+ * <Box p="4">
+  * <Blinker color="bg.brand.default" />
+ * </Box>
+ * ?)
+ */
 export const Blinker = ({
   color = '',
   size,
