1fc35f1d461619f71be81b33c17bb710d6869481
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-03-30 16:47:10 +0200

    draggable components documentaion

--- framework/lib/components/drag-and-drop/drag-item.tsx
+++ framework/lib/components/drag-and-drop/drag-item.tsx
@@ -4,6 +4,21 @@ import { Tag, TagLabel, TagRightIcon } from '../tag'
 import { Icon } from '../icon'
 import { DragItemProps } from './types'
 
+/**
+ * Default draggable item none other provided in SortableList and MultiSort.
+ *  Renders a tag which can be used under the DragAndDrop context
+ * @see DragAndDrop
+ * @see SortableList
+ * @see MultiSort
+ * @see {@link https://northlight.dev/reference/drag-item}
+ *
+ * @example (Example)
+ * Example:
+ * (?
+ * <DragItem itemLabel="Option 1" bgColor="bg.filled" />
+ * ?)
+ *
+ */
 export const DragItem = forwardRef(({
   size = 'md',
   isDragging,
--- framework/lib/components/drag-and-drop/droppable.tsx
+++ framework/lib/components/drag-and-drop/droppable.tsx
@@ -4,6 +4,42 @@ import { Box } from '../box'
 import { Center } from '../center'
 import { DroppableProps } from './types'
 
+/**
+ * Wrapper where you can style a container to place draggable elements
+ * @see DropZone
+ * @see Draggable
+ * @see DragAndDrop
+ * @see {@link https://northlight.dev/reference/droppable}
+ *
+ * @example (Example)
+ * ## Move the item into the container
+ * (?
+ * () => {
+ * const [ isDropped, setIsDropped ] = useState(false)
+ * const handleDragEnd = (e) => {
+ * setIsDropped(e.over && e.over.id === 'droppable')
+ * }
+ *
+ * return (
+ * <DragAndDrop onDragEnd={handleDragEnd}>
+ * <HStack>
+ * {!isDropped && <Draggable id="draggable" />}
+ * <Droppable id="droppable">
+ *  <Center bgColor="bg.filled" boxSize="3xs">
+ * {isDropped ? <Draggable id="draggable"/> :  <P>Drop here!</P>}
+ * </Center>
+ * </Droppable>
+ * </HStack>
+ *
+ * </DragAndDrop>
+ *
+ * )
+ *
+ *
+ * }
+ * ?)
+ *
+ */
 export const Droppable = ({ children, ...rest }: DroppableProps) => {
   const { setNodeRef, ...props } = useDroppable({
     ...rest,
