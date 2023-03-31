c220b1bc13c107a4e7e42c4f9f3520636fbfa353
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-03-31 15:56:35 +0200

    remove overflow group

--- framework/sandbox/docs/routes/routes.tsx
+++ framework/sandbox/docs/routes/routes.tsx
@@ -18,11 +18,6 @@ export const routes: Page[] = sortBy(prop('title'), [
     component: () => import('../pages/search-bar-page'),
   },
   {
-    title: 'Overflow Group',
-    path: '/overflow-group',
-    component: () => import('../pages/overflow-group-page'),
-  },
-  {
     title: 'Transitions',
     path: '/transitions',
     component: () => import('../pages/transitions-page'),
