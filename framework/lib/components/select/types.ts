cc6f02b74a73a304fcebb6a4d4f5bf42da2fb2ac
Anna Malmgren <anna@mediatool.com>
2023-04-11 15:08:54 +0200

    fix(framework): add type for custom option
    added missing type for customOption so that it can be used in typescript
    files.

--- framework/lib/components/select/types.ts
+++ framework/lib/components/select/types.ts
@@ -36,6 +36,7 @@ export interface SelectProps<T>
   | undefined
   /** Custom icon that will be put to the faremost right of the component */
   icon?: ComponentType<any>
+  customOption?: ((option: T) => JSX.Element) | null
 }
 
 export type SelectFieldProps<T> = Omit<SelectProps<T>, 'onChange'> &
