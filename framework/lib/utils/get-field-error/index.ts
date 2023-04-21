39653ac3c64686426a81c62375208f882242e44f
Orestis Rodriguez <orestis@mediatool.com>
2023-04-21 13:44:08 +0200

    fix(framework): replace wrong import of type

--- framework/lib/utils/get-field-error/index.ts
+++ framework/lib/utils/get-field-error/index.ts
@@ -1,6 +1,6 @@
 import { path, replace, split } from 'ramda'
 import { FieldErrorsImpl, FieldValues } from 'react-hook-form'
-import { FieldErrorType } from 'lib/components/form/types'
+import { FieldErrorType } from '../../components/form/types'
 
 export function getFieldError<T extends FieldValues> (name: string, errors: FieldErrorsImpl<T>) {
   const nameWithoutRightBracket = replace(/]/g, '', name)
