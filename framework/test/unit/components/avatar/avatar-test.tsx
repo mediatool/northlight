b7def12c318a85ba83b134629466dc644efe9f84
Orestis Rodriguez <orestis@mediatool.com>
2023-04-21 13:51:07 +0200

    chore: add a base tsconfig to the project

--- framework/test/unit/components/avatar/avatar-test.tsx
+++ framework/test/unit/components/avatar/avatar-test.tsx
@@ -2,7 +2,68 @@ import React from 'react'
 import { assert, expect } from 'chai'
 import { render, screen } from '@testing-library/react'
 import { Avatar } from '../../../../lib/components'
-import { users } from '../../../../sandbox/app/context'
+
+export interface User {
+  name: string
+  country: string
+  image: string
+  preferences: {
+    locale: string
+    currency: string
+    firstDayOfWeek: 'monday' | 'sunday'
+  }
+}
+
+const users: Record<string, User> = {
+  'obi-wan': {
+    name: 'Obi-Wan Kenobi',
+    country: 'Germany',
+    image: `
+      https://www.gamereactor.se/media/08/redaxresonerar_3470803.jpg
+    `,
+    preferences: {
+      locale: 'de-DE',
+      currency: 'EUR',
+      firstDayOfWeek: 'monday',
+    },
+  },
+  anakin: {
+    name: 'Anakin Skywalker',
+    country: 'US',
+    image: `
+      https://thenerdstash.com/wp-content/uploads/2021/10/Hayden-Christensen-Anakin-Skywalker.jpg
+    `,
+    preferences: {
+      locale: 'en-US',
+      currency: 'USD',
+      firstDayOfWeek: 'sunday',
+    },
+  },
+  han: {
+    name: 'Han Solo',
+    country: 'Sweden',
+    image: `
+      https://fictionhorizon.com/wp-content/uploads/2021/09/Han-Solo.jpg
+    `,
+    preferences: {
+      locale: 'sv-SE',
+      currency: 'SEK',
+      firstDayOfWeek: 'sunday',
+    },
+  },
+  yoda: {
+    name: 'Yoda',
+    country: 'England',
+    image: `
+      https://markerasomolast.files.wordpress.com/2013/08/yoda.jpg
+    `,
+    preferences: {
+      locale: 'en-GB',
+      currency: 'GBP',
+      firstDayOfWeek: 'monday',
+    },
+  },
+}
 
 const { isOk } = assert
 
