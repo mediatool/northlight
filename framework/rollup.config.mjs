6ac5354a2fc4238cfe18c9186c377c36d8b4672d
Orestis Rodriguez <orestis@mediatool.com>
2023-04-21 13:43:20 +0200

    fix(framework): exclude all dependencies from bundle

--- framework/rollup.config.mjs
+++ framework/rollup.config.mjs
@@ -1,64 +1,60 @@
 import { readFileSync } from 'fs'
-import image from '@rollup/plugin-image'
-import json from '@rollup/plugin-json'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
 
 const pkg = JSON.parse(readFileSync('./package.json', 'utf8'))
 
 const external = [
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.devDependencies),
   ...Object.keys(pkg.peerDependencies),
 ]
 
-export default [ {
-  input: './index.ts',
-  external,
-  plugins: [
-    json(),
-    commonjs(),
-    esbuild(),
-    resolve(),
-    image(),
-  ],
-  output: [ {
-    format: 'es',
-    sourcemap: true,
-    file: 'dist/es/mediatool-ui-framework.mjs',
-  }, {
-    format: 'umd',
-    name: 'mtUiFramework',
-    sourcemap: true,
-    file: 'dist/umd/mediatool-ui-framework.js',
-  } ],
-}, {
-  input: './index.ts',
-  external,
-  plugins: [
-    json(),
-    commonjs(),
-    esbuild({ minify: true }),
-    resolve(),
-    image(),
-  ],
-  output: [ {
-    format: 'umd',
-    name: 'mtUiFramework',
-    sourcemap: true,
-    file: 'dist/umd/mediatool-ui-framework.min.js',
-  } ],
-}, {
-  input: './index.ts',
-  external,
-  plugins: [
-    dts(),
-  ],
-  output: [ {
-    format: 'es',
-    file: 'dist/es/mediatool-ui-framework.d.mts',
-  }, {
-    format: 'cjs',
-    file: 'dist/ts/types.d.ts',
-  } ],
-} ]
+export default [
+  {
+    input: './index.ts',
+    external,
+    plugins: [ esbuild() ],
+    output: [
+      {
+        format: 'es',
+        sourcemap: true,
+        file: 'dist/es/northlight.mjs',
+      },
+      {
+        format: 'umd',
+        name: 'northlight',
+        sourcemap: true,
+        file: 'dist/umd/northlight.js',
+      },
+    ],
+  },
+  {
+    input: './index.ts',
+    external,
+    plugins: [ esbuild({ minify: true }) ],
+    output: [
+      {
+        format: 'umd',
+        name: 'northlight',
+        sourcemap: true,
+        file: 'dist/umd/northlight.min.js',
+      },
+    ],
+  },
+  {
+    input: './index.ts',
+    external,
+    plugins: [ dts() ],
+    output: [
+      {
+        format: 'es',
+        file: 'dist/es/northlight.d.mts',
+      },
+      {
+        format: 'cjs',
+        file: 'dist/ts/types.d.ts',
+      },
+    ],
+  },
+]
