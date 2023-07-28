fc93a202a349aa8702ecdb5ec577868ec1914c03
ilija- <ilijatp@gmail.com>
2023-07-28 13:12:28 +0200

    chore(northlight): new labelSize & labelPlacement props for CheckboxField
    - Added 2 new props for better design handling
    - Code updated for better responsiveness
    - Documentation with example where you can play around
    
    closes: DEV-8668

--- framework/lib/components/checkbox/checkbox-field.tsx
+++ framework/lib/components/checkbox/checkbox-field.tsx
@@ -1,42 +1,73 @@
 import React from 'react'
+import { identity } from 'ramda'
 import { CheckboxFieldProps } from './types'
 import { Field } from '../form'
 import { Checkbox } from './checkbox'
 import { Box } from '../box'
+import { Label } from '../typography'
+import { Flex } from '../flex'
 
 /**
  * The checkbox component wrapped in a <Field />
  * meant to be used only inside <Form />
  * @see Checkbox
  * @see {@link https://northlight.dev/reference/checkbox-field}
+ * @example (Example)
+ * ## Basic
+ * (?
+ * <Form initialValues={{name: ''}}>
+ *  <CheckboxField
+ *   name="terms"
+ *   label="I agree to the Terms & Conditions"
+ *   labelPlacement="left"
+ *   labelSize="md"
+ *  />
+ * </Form>
+ * ?)
  *
  */
+
 export const CheckboxField = ({
   name,
   label,
   variant,
   isRequired,
   validate,
+  onChange: onChangeCallback = identity,
   direction = 'row',
+  labelPlacement = 'left',
+  labelSize = 'md',
   ...rest
 }: CheckboxFieldProps) => (
-  <Box w={ label ? 'full' : 'fit-content' }>
+  <Box
+    w={ label ? 'full' : 'fit-content' }
+    display="inline-flex"
+  >
     <Field
       name={ name }
-      label={ label }
-      direction={ direction }
+      label=""
       isRequired={ isRequired }
+      direction={ direction }
       validate={ validate }
     >
-      { (field) => (
-        <Checkbox
-          name={ name }
-          data-testid="checkbox-field-test-id"
-          onChange={ field.onChange }
-          value={ field.value }
-          variant={ variant }
-          { ...rest }
-        />
+      { ({ onChange, value }) => (
+        <Flex
+          gap={ 2 }
+          direction={ labelPlacement === 'left' ? 'row-reverse' : 'row' }
+        >
+          <Checkbox
+            name={ name }
+            onChange={ (e) => {
+              onChange(e)
+              onChangeCallback(e)
+            } }
+            value={ value }
+            variant={ variant }
+            data-testid="checkbox-field-test-id"
+            { ...rest }
+          />
+          <Label htmlFor={ name } sx={ { fontWeight: 'normal' } } size={ labelSize }>{ label }</Label>
+        </Flex>
       ) }
     </Field>
   </Box>
