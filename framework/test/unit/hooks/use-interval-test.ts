402d91b63a4aa0e236bf78fbebd7704daedc3438
Jonathan Neville <jonathanneville@Jonathans-Air.localdomain>
2023-08-08 11:24:40 +0200

    feat(framework): add hooks for debounce, interval and loading message
    closes: DEV-8737

---
+++ framework/test/unit/hooks/use-interval-test.ts
@@ -0,0 +1,22 @@
+import { renderHook, waitFor } from '@testing-library/react'
+import { assert } from 'chai'
+import { useLoadingMessage } from '../../../lib/hooks/use-loading-message'
+
+describe('useLoadingMessage', () => {
+  it('returns the value and adds a dot to it every 300 ms', async () => {
+    const { result } = renderHook(
+      (v) => useLoadingMessage(v),
+      { initialProps: 'loading' }
+    )
+
+    assert.equal(result.current, 'loading.')
+
+    await waitFor(() => {
+      assert.equal(result.current, 'loading..')
+    }, { interval: 350 })
+
+    await waitFor(() => {
+      assert.equal(result.current, 'loading...')
+    }, { interval: 350 })
+  })
+})
