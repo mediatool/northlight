ae22d7a7e8d3fc16948a9280e0fe8403a6a9290d
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-09-11 17:42:07 +0200

    fix(framework): add leftIcon prop to select
    closed DEV-9169

--- framework/lib/components/search-bar/get-components.tsx
+++ framework/lib/components/search-bar/get-components.tsx
@@ -1,46 +1,58 @@
 import React from 'react'
 import {
+  ControlProps,
   DropdownIndicatorProps,
   GroupBase,
   MultiValueGenericProps,
   OptionProps,
   chakraComponents,
 } from 'chakra-react-select'
+import { HStack } from '../stack'
 import { Icon } from '../icon'
 import { SearchBarOptionType } from './types'
 
 export function getComponents<T extends SearchBarOptionType> () {
   return {
-    DropdownIndicator: (props: DropdownIndicatorProps<T>) => (
-      props.selectProps.icon
-        ? (
-          <chakraComponents.DropdownIndicator { ...props }>
-            <Icon as={ props.selectProps.icon } />
-          </chakraComponents.DropdownIndicator>
-        )
-        : (
-          <chakraComponents.DropdownIndicator { ...props } />
-        )
-    ),
+    DropdownIndicator: (props: DropdownIndicatorProps<T>) =>
+      (props.selectProps.icon ? (
+        <chakraComponents.DropdownIndicator { ...props }>
+          <Icon as={ props.selectProps.icon } />
+        </chakraComponents.DropdownIndicator>
+      ) : (
+        <chakraComponents.DropdownIndicator { ...props } />
+      )),
     Option: (props: OptionProps<T>) =>
-      (props.selectProps.customOption
-        ? (
-          <chakraComponents.Option { ...props }>
-            { props.selectProps.customOption(props.data) }
-          </chakraComponents.Option>
-        )
-        : <chakraComponents.Option { ...props } />
-      ),
+      (props.selectProps.customOption ? (
+        <chakraComponents.Option { ...props }>
+          { props.selectProps.customOption(props.data) }
+        </chakraComponents.Option>
+      ) : (
+        <chakraComponents.Option { ...props } />
+      )),
     MultiValueContainer: (
       props: MultiValueGenericProps<T, boolean, GroupBase<T>>
     ) =>
-      (props.selectProps.customTag
-        ? (
-          <chakraComponents.MultiValueContainer { ...props }>
-            { props.selectProps.customTag(props.data) }
-          </chakraComponents.MultiValueContainer>
-        )
-        : <chakraComponents.MultiValueContainer { ...props } />
-      ),
+      (props.selectProps.customTag ? (
+        <chakraComponents.MultiValueContainer { ...props }>
+          { props.selectProps.customTag(props.data) }
+        </chakraComponents.MultiValueContainer>
+      ) : (
+        <chakraComponents.MultiValueContainer { ...props } />
+      )),
+    Control: ({ children, ...props }: ControlProps<T, boolean, GroupBase<T>>) =>
+      (props.selectProps.leftIcon ? (
+        <chakraComponents.Control { ...props }>
+          <HStack w="full" pl="2">
+            <Icon as={ props.selectProps.leftIcon } />
+            <HStack w="full" justify="space-between">
+              { children }
+            </HStack>
+          </HStack>
+        </chakraComponents.Control>
+      ) : (
+        <chakraComponents.Control { ...props }>
+          { children }
+        </chakraComponents.Control>
+      )),
   }
 }
