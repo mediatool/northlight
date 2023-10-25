d8e5a827158ab26918e5b346d670a50b122b6061
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-10-25 16:42:09 +0200

    fix(framework): change chakra ui spinner to northlight spinner
    The spinner that was used for the loading state of the button was actually
    the default spinner provided by Chakra UI. By chaning the default spinner
    for our own spinner this fixed the issue
    
    closed: DEV-9732

--- framework/lib/components/button/types.ts
+++ framework/lib/components/button/types.ts
@@ -3,8 +3,9 @@ import { ComponentProps } from 'react'
 
 export type ButtonVariants = 'default' | 'danger' | 'success' | 'brand' | 'brandSubdued' | 'link' | 'ghost'
 
-export type ButtonProps<As extends React.ElementType> = Omit<ChakraButtonProps, 'as'> & GenericAsProps<As> & {
+export type ButtonProps<As extends React.ElementType> = Omit<ChakraButtonProps, 'as' | 'size'> & Omit<GenericAsProps<As>, 'size'> & {
   variant?: ButtonVariants
+  size?: 'xs' | 'sm' | 'md' | 'lg'
 }
 
 export type GenericAsProps<As extends React.ElementType> = (
