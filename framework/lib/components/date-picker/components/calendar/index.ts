7bec8bbf02099c376dd8cec9366bf79493eb9e49
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-11-02 17:03:50 +0100

    fix(framework): export date-picker comopnents

--- framework/lib/components/date-picker/components/calendar/index.ts
+++ framework/lib/components/date-picker/components/calendar/index.ts
@@ -1,1 +1,4 @@
 export * from './calendar'
+export * from './range-calendar'
+export * from './simple-range-calendar'
+export * from './date-select'
--- framework/lib/components/date-picker/index.ts
+++ framework/lib/components/date-picker/index.ts
@@ -1,3 +1,4 @@
 export * from './date-picker'
 export * from './date-picker-field'
+export * from './components'
 export * from './types'
