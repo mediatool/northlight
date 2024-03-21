d4a1f1503586bb5fb28cdd7c4e4c7f45530b59c0
Gustaf Grönlund <gustafgronlund@gmail.com>
2024-03-21 16:51:23 +0100

    feat(framework): add new editabletext variant

--- framework/lib/components/editable-text/types.ts
+++ framework/lib/components/editable-text/types.ts
@@ -3,11 +3,15 @@ import { EditableProps as ChakraEditableProps } from '@chakra-ui/react'
 
 export type EditableSizes = 'sm' | 'md' | 'lg'
 
+export type EditableVariant = 'brand' | 'default'
+
 export interface EditableProps extends ChakraEditableProps {
   value?: string
   size?: EditableSizes
   leftItem? : ReactElement
+  variant?: EditableVariant
 }
 export interface EditableControlsProps {
   size: EditableSizes
+  variant: EditableVariant
 }
