4aee0de5895449092b96efe47c88cda1a4381915
Sebastian Delgado <sebastiandelgado@Sebastians-Laptop.local>
2024-05-23 16:40:53 +0200

    fix(framework): fix oveflowing daylabels

--- framework/lib/components/date-picker/components/calendar/components/day-labels.tsx
+++ framework/lib/components/date-picker/components/calendar/components/day-labels.tsx
@@ -2,20 +2,17 @@ import React, { memo } from 'react'
 import { chakra, useMultiStyleConfig } from '@chakra-ui/system'
 import { DayLabelsProps } from './types'
 
-export const DayLabels = memo(({
-  weekDays,
-}: DayLabelsProps) => {
+export const DayLabels = memo(({ weekDays }: DayLabelsProps) => {
   const { dayLabel } = useMultiStyleConfig('Calendar')
   const weekDaysWithIds = weekDays.map((day, i) => ({ label: day, _id: i }))
 
   return (
-    <chakra.tr>
-      <chakra.th />
+    <>
       { weekDaysWithIds.map(({ label, _id }) => (
         <chakra.th key={ _id } __css={ dayLabel }>
           { label }
         </chakra.th>
       )) }
-    </chakra.tr>
+    </>
   )
 })
