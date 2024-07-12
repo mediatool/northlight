947026aabc2fa9aded9e673d826bea441f30a554
Marc Biemer <marc.biemer@mediatool.com>
2024-07-12 12:52:23 +0200

    chore(northlight): Add semantic colors and consistent padding to StatusPin
    Updated innerPin size for lg to be 4 instead of 3.5 to improve consistency of padding and scaling between size variants.
    Adjusted colorVariant mapping to use semantic tokens instead of green.500 etc.
    Updated documentation by adding more details, linking to related badge component and improving the usage presentation.
    
    Closes: DEV-14603

--- framework/lib/components/status-pin/pin-variant-map.ts
+++ framework/lib/components/status-pin/pin-variant-map.ts
@@ -1,13 +1,12 @@
-import { Color, ColorGrade } from '../../types'
 import { PinVariant } from '../pin-input/types'
 
-export const pinVariantMap: Record<PinVariant, `${Color}.${ColorGrade}`> = {
-  green: 'green.500',
-  running: 'green.500',
-  yellow: 'yellow.600',
-  inProgress: 'yellow.600',
-  gray: 'gray.300',
-  notExecuted: 'gray.300',
-  red: 'red.500',
-  rejected: 'red.500',
+export const pinVariantMap: Record<PinVariant, string> = {
+  green: 'success',
+  running: 'success',
+  yellow: 'info',
+  inProgress: 'info',
+  gray: 'subdued',
+  notExecuted: 'subdued',
+  red: 'destructive',
+  rejected: 'destructive',
 }
