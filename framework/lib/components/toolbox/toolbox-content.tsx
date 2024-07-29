0807ef02454937e832cdc4a469e0b311ad1524e9
Gustav H Frithiof <gustav.harlenback.frithiof@mediatool.com>
2024-07-29 17:19:45 +0200

    chore(framework): add sx to toolbox content
    Toolbox content accepts sx as prop in order to be able
    to override styling. Changed children wrapper in toolbox
    from box to vstack to be able to use flex grow for children

--- framework/lib/components/toolbox/toolbox-content.tsx
+++ framework/lib/components/toolbox/toolbox-content.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useMultiStyleConfig } from '@chakra-ui/system'
+import { merge } from 'ramda'
 import { Flex } from '../flex'
 import { ToolboxContentProps } from './types'
 
@@ -8,12 +9,12 @@ import { ToolboxContentProps } from './types'
  * @see Toolbox
  * @see {@link https://northlight.dev/reference/toolbox-content}
  */
-export const ToolboxContent = ({ children, ...rest }: ToolboxContentProps) => {
+export const ToolboxContent = ({ sx = {}, children, ...rest }: ToolboxContentProps) => {
   const { body } = useMultiStyleConfig('Toolbox', {})
 
   return (
     <Flex
-      sx={ body }
+      sx={ merge(body, sx) }
       { ...rest }
     >
       { children }
