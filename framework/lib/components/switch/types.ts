097f6c39c3ed94d06a8f5f8bfdac3306cfa6d1c2
SebCodesTheWeb <zenrigod@gmail.com>
2024-11-12 13:59:23 +0100

    fix(framework): use interface instead of type for swich types
    This is for the auto generated documentation
    to find the correct types for switch

--- framework/lib/components/switch/types.ts
+++ framework/lib/components/switch/types.ts
@@ -2,21 +2,17 @@ import { SwitchProps as ChakraSwitchProps, StackDirection } from '@chakra-ui/rea
 import { ChangeEvent } from 'react'
 import { RegisterOptions } from 'react-hook-form'
 
-export type SwitchProps =
-  ChakraSwitchProps
-  & {
-    onChange?: (e: ChangeEvent<HTMLInputElement>) => void
-    value?: boolean
-  }
+export interface SwitchProps extends Omit<ChakraSwitchProps, 'value'> {
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void
+  value?: boolean
+}
 
-export type SwitchFieldProps =
-  ChakraSwitchProps
-  & {
-    name: string
-    label: string
-    validate?: RegisterOptions
-    isRequired?: boolean
-    direction?: StackDirection
-    labelPlacement?: 'left' | 'right'
-    labelSize?: '2xs' | 'xs' | 'md' | 'lg'
-  }
+export interface SwitchFieldProps extends ChakraSwitchProps {
+  name: string
+  label: string
+  validate?: RegisterOptions
+  isRequired?: boolean
+  direction?: StackDirection
+  labelPlacement?: 'left' | 'right'
+  labelSize?: '2xs' | 'xs' | 'md' | 'lg'
+}
