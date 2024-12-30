79b6c32bff3205f136cd5072064793005218abc4
SebCodesTheWeb <zenrigod@gmail.com>
2024-12-30 16:28:29 +0100

    fix(framework): add forwardRef to stylized typography components and p
    Otherwise it's not possible for charka ui tooltip
    to attach the tooltip properly

--- framework/lib/components/typography/types.ts
+++ framework/lib/components/typography/types.ts
@@ -1,10 +1,11 @@
-import { Heading, Text } from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react'
 import { P } from './body'
 import { Label } from './labels'
 import { NumVal } from './numvals'
+import { Blockquote } from './stylized'
 
 export type HeadingType = typeof Heading
 export type BodyType = typeof P
-export type StylizedType = typeof Text
+export type StylizedType = typeof Blockquote
 export type LabelType = typeof Label
 export type NumValType = typeof NumVal
