c03869295af936e3164a871c8e49c94c87cf1ebb
Ilija <ilijatp@gmail.com>
2025-05-21 13:15:33 +0200

    feat(northlight): alert doc page
    Adds usage page for the Alert component

--- framework/lib/components/alert/alert.tsx
+++ framework/lib/components/alert/alert.tsx
@@ -2,6 +2,57 @@ import React from 'react'
 import { Alert as ChakraAlert } from '@chakra-ui/react'
 import { AlertProps } from './types'
 
+/**
+ * @example (Example)
+ * ## Usage
+ * <br />
+ * Northlight exports 4 alert related components: <br />
+ * <br />
+ * `Alert`: The wrapper for alert components. <br />
+ * `AlertIcon`: The visual icon for the alert that changes based on the status prop. <br />
+ * `AlertTitle`: The title of the alert to be announced by screen readers. <br />
+ * `AlertDescription`: The description of the alert to be announced by screen readers. <br />
+ * <br />
+ * (?
+ * +
+ * const toastIconMap = {
+ *   success: CheckCircleSolid,
+ *   warning: AlertTriangleSolid,
+ *   error: AlertCircleSolid,
+ *   danger: AlertCircleSolid,
+ *   info: BellSolid,
+ *   ai: BrightnessSolid,
+ *   default: HelpCircleSolid,
+ *   ghost: HelpCircleSolid,
+ * }
+ *
+ * const variants = [
+ *   "default",
+ *   "success",
+ *   "warning",
+ *   "error",
+ *   "info",
+ *   "ai",
+ *   "ghost",
+ * ]
+ *
+ * const Example = () => (
+ *   <SimpleGrid columns={2} columnGap="2" rowGap="4">
+ *
+ *     {variants.map((variant) => (
+ *       <Alert variant={variant}>
+ *         <AlertIcon as={toastIconMap[variant]} />
+ *         <AlertTitle>Title</AlertTitle>
+ *         <AlertDescription>Description</AlertDescription>
+ *       </Alert>
+ *     ))}
+ *   </SimpleGrid>
+ * )
+ * render(<Example />)
+
+ * ?)
+ */
+
 export const Alert = ({
   variant = 'success',
   children,
