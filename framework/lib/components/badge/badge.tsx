73dde7889357ba1a0008e82ba6d0b8430d5c5c70
Ilija <ilijatp@gmail.com>
2025-05-21 13:15:33 +0200

    feat(northlight): badge icon
    This commit adds possibility to add icon and iconPosition to a badge

--- framework/lib/components/badge/badge.tsx
+++ framework/lib/components/badge/badge.tsx
@@ -1,33 +1,55 @@
 import React, { forwardRef } from 'react'
 import { Badge as ChakraBadge } from '@chakra-ui/react'
+import { MediatoolLogoSolid } from '@northlight/icons'
 import { BadgeProps } from './types'
+import { Flex } from '../flex'
+import { Icon } from '../icon'
 
 /**
- * Badges are used to highlight an item's status for quick recognition.
- * @see {@link https://northlight.dev/reference/badge}
- *
  * @example
  * (?
  * +
  * const colors = ["gray", "mediatoolBlue", "blue", "red", "green",
  * "orange", "yellow", "teal", "purple", "pink"]
  * const variants = ["solid", "outline", "subtle", "ghost"]
- * const Example = () => {
- *     return <Stack>
- *         { colors.map((color) => (
- *         <HStack spacing={ 4 }>
- *          {
- *              variants.map((variant) => (
- *                    <Badge colorScheme={color} variant={variant}>{variant} Badge</Badge>
- *              ))
- *          }
- *         </HStack>
+ *
+ * const Example = () => (
+ *   <Stack>
+ *     {colors.map((color, i) => (
+ *       <HStack spacing={4} key={i}>
+ *         {variants.map((variant) => (
+ *           <Badge
+ *             colorScheme={color}
+ *             variant={variant}
+ *             iconPosition="left"
+ *             withIcon
+ *           >
+ *             {variant} Badge
+ *           </Badge>
  *         ))}
- *     </Stack>
- * }
- * render(<Example/>)
+ *       </HStack>
+ *     ))}
+ *   </Stack>
+ * )
+ * render(<Example />)
  * ?)
  */
-export const Badge = forwardRef<HTMLDivElement, BadgeProps>((props, ref) => (
-  <ChakraBadge ref={ ref } { ...props } />
-))
+
+export const Badge = forwardRef<HTMLDivElement, BadgeProps>((props, ref) => {
+  const {
+    children,
+    withIcon,
+    iconPosition = 'left',
+    ...rest
+  } = props
+
+  return (
+    <ChakraBadge ref={ ref } { ...rest }>
+      <Flex align="center" gap="1">
+        { withIcon && iconPosition === 'left' && <Icon as={ MediatoolLogoSolid } boxSize="3" /> }
+        { children }
+        { withIcon && iconPosition === 'right' && <Icon as={ MediatoolLogoSolid } boxSize="3" /> }
+      </Flex>
+    </ChakraBadge>
+  )
+})
