409a4ba5c9662ad06ec0cbff40ed7055a0d121b4
Alexandros Karakitsos <aakarakitsos@gmail.com>
2026-01-14 16:55:39 +0100

    feat(framework): export toggle function from use disclosure Added a toggle function from use disclosure. In a lot of cases, users of this hook build a custom onToggle function for every use disclosure usage.

--- framework/lib/hooks/use-disclosure/index.ts
+++ framework/lib/hooks/use-disclosure/index.ts
@@ -1,3 +1,12 @@
-export {
-  useDisclosure,
+import {
+  useDisclosure as useDisclosureChakra,
 } from '@chakra-ui/react'
+import type { UseDisclosureProps } from '@chakra-ui/react'
+
+export const useDisclosure = (props?: UseDisclosureProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosureChakra(props)
+
+  const onToggle = () => (isOpen ? onClose() : onOpen())
+
+  return { isOpen, onOpen, onClose, onToggle }
+}
