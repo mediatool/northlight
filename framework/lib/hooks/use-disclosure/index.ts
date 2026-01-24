5cea2a9c53f085890dbebfcbe5c2be9edd17fca3
Sebastian <90923099+SebCodesTheWeb@users.noreply.github.com>
2026-01-24 11:04:15 +0100

    Revert "feat(framework): export toggle function from use disclosure"
    This reverts commit 55f48bb643ed152b0d4bbeb65c61e28d4d546b10.

--- framework/lib/hooks/use-disclosure/index.ts
+++ framework/lib/hooks/use-disclosure/index.ts
@@ -1,12 +1,3 @@
-import {
-  useDisclosure as useDisclosureChakra,
+export {
+  useDisclosure,
 } from '@chakra-ui/react'
-import type { UseDisclosureProps } from '@chakra-ui/react'
-
-export const useDisclosure = (props?: UseDisclosureProps) => {
-  const { isOpen, onOpen, onClose } = useDisclosureChakra(props)
-
-  const onToggle = () => (isOpen ? onClose() : onOpen())
-
-  return { isOpen, onOpen, onClose, onToggle }
-}
