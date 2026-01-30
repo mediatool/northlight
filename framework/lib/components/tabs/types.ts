c4199284aaf0aeab739dec0d5b4111f481bf902b
Ilija <ilijatp@gmail.com>
2026-01-30 15:57:26 +0100

    feat(northlight): new tabs variant
    Adds a new piped tabs variant to selection
    
    closes: DEV-24233

--- framework/lib/components/tabs/types.ts
+++ framework/lib/components/tabs/types.ts
@@ -1,4 +1,12 @@
-import { TabPanelProps as ChakraTabPanelProps, TabsProps as ChakraTabsProps } from '@chakra-ui/react'
+import {
+  TabPanelProps as ChakraTabPanelProps,
+  TabProps as ChakraTabProps,
+  TabsProps as ChakraTabsProps,
+} from '@chakra-ui/react'
 
 export interface TabsProps extends ChakraTabsProps {}
 export interface TabPanelProps extends ChakraTabPanelProps {}
+export interface TabProps extends ChakraTabProps {
+  /** Custom color for the indicator element (used with 'piped' variant) */
+  elementColor?: string
+}
