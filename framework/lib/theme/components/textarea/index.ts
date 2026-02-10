e6a00723cb63d618c77a30e2ee48ff5762a07338
Ilija <ilijatp@gmail.com>
2026-02-10 09:01:24 +0100

    fix(northlight): text area outline
    Fixes the issue with the outline/border color where it remains black
    
    closes: DEV-14260

--- framework/lib/theme/components/textarea/index.ts
+++ framework/lib/theme/components/textarea/index.ts
@@ -2,58 +2,99 @@ import { ComponentSingleStyleConfig } from '@chakra-ui/react'
 
 export const Textarea: ComponentSingleStyleConfig = {
   baseStyle: ({ theme: {
-    space: spacing,
     colors: color,
-    borders: borderWidth,
-    radii: borderRadius,
   } }) => ({
-    paddingInlineStart: spacing['padding-inline'].textarea.default,
-    paddingInlineEnd: spacing['padding-inline'].textarea.default,
-    borderRadius: borderRadius.textarea.default,
-    borderWidth: borderWidth.textarea.default,
-    borderColor: color.border.textarea.default,
     color: color.text.default,
-    bgColor: color.background.textarea.default,
-    _hover: {
-      borderColor: color.border.textarea['default-hover'],
-      bgColor: color.background.textarea.hover,
-    },
-    _focusVisible: {
-      borderColor: color.border.textarea.focus,
-      boxShadow: `0 0 0 1px ${color.border.textarea.focus}`,
-      bgColor: color.background.textarea.active,
-    },
-    _invalid: {
-      boxShadow: 'none',
-      borderColor: color.border.textarea.error,
-      bgColor: color.background.textarea.error,
-    },
-    _readOnly: {
-      _focusVisible: {
-        borderColor: color.border.textarea.readonly,
-        bgColor: color.background.textarea.default,
-      },
-      _hover: {
-        borderColor: color.border.textarea.readonly,
-      },
-    },
-    _disabled: {
-      bgColor: color.background.textarea.disabled,
-      _hover: {
-        borderColor: color.border.textarea.default,
-      },
-    },
     _placeholder: {
       color: color.text.textarea.placeholder,
     },
   }),
   variants: {
-    outline: {},
-    ai: ({ theme: { colors: color } }) => ({
+    outline: ({ theme: {
+      space: spacing,
+      colors: color,
+      borders: borderWidth,
+      radii: borderRadius,
+    } }) => ({
+      paddingInlineStart: spacing['padding-inline'].textarea.default,
+      paddingInlineEnd: spacing['padding-inline'].textarea.default,
+      borderRadius: borderRadius.textarea.default,
+      borderWidth: borderWidth.textarea.default,
+      borderColor: color.border.textarea.default,
+      bgColor: color.background.textarea.default,
+      _hover: {
+        borderColor: color.border.textarea['default-hover'],
+        bgColor: color.background.textarea.hover,
+      },
       _focusVisible: {
+        borderColor: color.border.textarea.focus,
+        boxShadow: `0 0 0 1px ${color.border.textarea.focus}`,
+        bgColor: color.background.textarea.active,
+      },
+      _invalid: {
+        boxShadow: 'none',
+        borderColor: color.border.textarea.error,
+        bgColor: color.background.textarea.error,
+      },
+      _readOnly: {
+        _focusVisible: {
+          borderColor: color.border.textarea.readonly,
+          bgColor: color.background.textarea.default,
+        },
         _hover: {
-          boxShadow: `0 0 0 1px ${color.border.ai}`,
-          borderColor: color.border.ai,
+          borderColor: color.border.textarea.readonly,
+        },
+      },
+      _disabled: {
+        bgColor: color.background.textarea.disabled,
+        _hover: {
+          borderColor: color.border.textarea.default,
+        },
+      },
+    }),
+    ai: ({ theme: {
+      space: spacing,
+      colors: color,
+      borders: borderWidth,
+      radii: borderRadius,
+    } }) => ({
+      paddingInlineStart: spacing['padding-inline'].textarea.default,
+      paddingInlineEnd: spacing['padding-inline'].textarea.default,
+      borderRadius: borderRadius.textarea.default,
+      borderWidth: borderWidth.textarea.default,
+      borderColor: color.border.textarea.default,
+      bgColor: color.background.textarea.default,
+      _hover: {
+        borderColor: color.border.textarea['default-hover'],
+        bgColor: color.background.textarea.hover,
+      },
+      _focusVisible: {
+        bgColor: color.background.textarea.active,
+        boxShadow: `inset 0 0 0 1px ${color.border.ai}`,
+        borderColor: color.border.ai,
+        _invalid: {
+          boxShadow: `inset 0 0 0 1px ${color.border.textarea.error}`,
+          bgColor: color.background.textarea.error,
+          borderColor: color.border.textarea.error,
+        },
+      },
+      _invalid: {
+        bgColor: color.background.textarea.error,
+        borderColor: color.border.textarea.error,
+      },
+      _readOnly: {
+        _focusVisible: {
+          borderColor: color.border.textarea.readonly,
+          bgColor: color.background.textarea.default,
+        },
+        _hover: {
+          borderColor: color.border.textarea.readonly,
+        },
+      },
+      _disabled: {
+        bgColor: color.background.textarea.disabled,
+        _hover: {
+          borderColor: color.border.textarea.default,
         },
       },
     }),
