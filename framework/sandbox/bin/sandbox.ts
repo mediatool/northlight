c978720bdf01c74175773590f35e92aedfdc20ca
Magnus Ohlin <magnus@mediatool.com>
2026-02-16 14:10:57 +0100

    fix(sandbox): font loading in sandbox
    this adds loading of google fonts to the sandbox html

--- framework/sandbox/bin/sandbox.ts
+++ framework/sandbox/bin/sandbox.ts
@@ -80,6 +80,7 @@ async function main () {
                 <head>
                   <meta charset="UTF-8" />
                   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
                   <title>Sandbox</title>
                 </head>
                 <body>
@@ -107,6 +108,7 @@ async function main () {
                   <head>
                     <meta charset="UTF-8" />
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
                     <title>Sandbox</title>
                   </head>
                   <body>
